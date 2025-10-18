4b0d61c669050df2b065c51274cd5dc95c358c87
minhtuyt8899 <minhtuemailverify@gmail.com>
2025-10-18 19:09:24 +0700

    Update caidat2.sh

--- caidat2.sh
+++ caidat2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "[i] Chúng Tôi Sẽ Cài Đặt XyZmtu..."
-if [ -d "/usr/share/xyzmtu" ] ; then
+if [ -d "/usr/share-main/xyzmtu" ] ; then
     echo "[i] Đã Tìm Thấy Bản Cũ Của XyZmtu, Đang Update Lên Bản Mới..."
     echo "[i] Backup Bản Cũ."
-    if [ -d "/usr/share/xyzmtu/Backup" ] ; then
-        sudo mv /usr/share/xyzmtu/Backup ./Backup
+    if [ -d "/usr/share/xyzmtu-main/Backup" ] ; then
+        sudo mv /usr/share/xyzmtu-main/Backup ./Backup
     else
         mkdir ./Backup
     fi
@@ -16,21 +16,21 @@ if [ -d "/usr/share/xyzmtu" ] ; then
         done
             name="$name.bak$i"
     fi
-    sudo mv /usr/share/xyzmtu $name
+    sudo mv /usr/share/xyzmtu-main $name
     mv ./Backup ./xyzmtu/
     sudo cp -ar .xyzmtu /usr/share/
     echo "[i] Cài Đặt Thành CÔng."
     echo "[i] Đang Làm XyZmtu Có Thể Thực Thi..."
-    sudo mv /usr/share/xyzmtu/main.py /usr/share/xyzmtu/xyzm
-    sudo chmod +x /usr/share/xyzmtu/xyzm
-    sudo ln -s /usr/share/xyzmtu/xyzm /usr/bin/xyzm || echo "[i] Đường Dẫn Đã Tồn Tại!."
+    sudo mv /usr/share/xyzmtu-main/main.py /usr/share/xyzmtu-main/xyzm
+    sudo chmod +x /usr/share/xyzmtu-main/xyzm
+    sudo ln -s /usr/share/xyzmtu-main/xyzm /usr/bin/xyzm || echo "[i] Đường Dẫn Đã Tồn Tại!."
 else
     sudo cp -ar ./xyzmtu /usr/share/
     echo "[i] Cài Đặt Thành Công!."
     echo "[i] Đang Làm XyZmtu Có Thể Thực Thi..."
-    sudo mv /usr/share/xyzmtu/main.py /usr/share/xyzmtu/xyzm
-    sudo chmod +x /usr/share/xyzmtu/xyzm
-    sudo ln -s /usr/share/xyzmtu/xyzm /usr/bin/xyzm || echo "[i] Dường Như Đường Đẫn Đã Tồn Tại!."
+    sudo mv /usr/share/xyzmtu-main/main.py /usr/share/xyzmtu-main/xyzm
+    sudo chmod +x /usr/share/xyzmtu-main/xyzm
+    sudo ln -s /usr/share/xyzmtu-main/xyzm /usr/bin/xyzm || echo "[i] Dường Như Đường Đẫn Đã Tồn Tại!."
 fi
 
 echo "[i] Bạn Cũng Có Thể Xóa Thư Mục XyZmtu Ngay Bây Giờ."
