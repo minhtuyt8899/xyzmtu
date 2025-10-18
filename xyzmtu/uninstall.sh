2311291a023e432ef20ff27f6206871b01e26cab
minhtuyt8899 <minhtuemailverify@gmail.com>
2025-10-18 19:25:13 +0700

    Update uninstall.sh

--- xyzmtu/uninstall.sh
+++ xyzmtu/uninstall.sh
@@ -1,7 +1,7 @@
 echo "[i] Chúng Tôi Sẽ Xóa XyZmtu..."
 echo "[i] Nó Sẽ Xóa Hết Tất Cả Backup."
 sudo rm -i /usr/bin/xyzm
-sudo rm -rf -i /usr/share/xyzmtu-main
+sudo rm -rf -i /usr/share/xyzmtu
 
 echo "[i] XyZmtu Đã Được Xóa Thành Công!."
 exit 0
